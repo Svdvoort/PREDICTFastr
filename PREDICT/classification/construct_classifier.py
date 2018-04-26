Fixed some bugs. Changed LoG feature computation to per 2D slice. Added PREDICT exceptions and an error catch.

PREDICT/classification/construct_classifier.py
@@ -110,7 +110,7 @@ def construct_SVM(config, regression=False):
     else:
         clf = SVMR(max_iter=100000)
 
-    if config['Classification']['Kernel'] == "polynomial":
+    if config['Classification']['Kernel'] == "polynomial" or config['Classification']['Kernel'] == "poly":
         param_grid = {'kernel': ['poly'], 'C': scipy.stats.uniform(loc=0.5e8, scale=0.5e8), 'degree': scipy.stats.uniform(loc=3, scale=2), 'coef0': scipy.stats.uniform(loc=0.5, scale=0.5)}
 
     elif config['Classification']['Kernel'] == "linear":
@@ -118,5 +118,7 @@ def construct_SVM(config, regression=False):
 
     elif config['Classification']['Kernel'] == "rbf":
         param_grid = {'kernel': ['rbf'], 'gamma':  scipy.stats.uniform(loc=0.5e-3, scale=0.5e-3), 'nu': scipy.stats.uniform(loc=0.5, scale=0.5)}
+    else:
+        raise KeyError("{} is not a valid SVM kernel type!").format(config['Classification']['Kernel'])
 
     return clf, param_grid

PREDICT/classification/crossval.py
@@ -332,17 +332,37 @@ def nocrossval(config, label_data_train, label_data_test, image_features_train,
         # label is maintained
         X_train = image_features_train
         X_test = image_features_test
-        Y_train = label_value_train
-        Y_test = label_value_test
+        Y_train = label_value_train.ravel()
+        Y_test = label_value_test.ravel()
 
         if config['SampleProcessing']['SMOTE']:
+            print("Sampling with SMOTE.")
+            for num, x in enumerate(X_train):
+                if num == 0:
+                    X_train_temp = np.zeros((len(x[0]), 1))
+                    X_train_temp[:, 0] = np.asarray(x[0])
+                else:
+                    xt = np.zeros((len(x[0]), 1))
+                    xt[:, 0] = np.asarray(x[0])
+                    X_train_temp = np.column_stack((X_train_temp, xt))
+
+            X_train_temp = np.transpose(X_train_temp)
             config_gen = config_io.load_config()
             N_jobs = config_gen['Joblib']['ncores']
             sm = SMOTE(random_state=random_state,
                        ratio=config['SampleProcessing']['SMOTE_ratio'],
+                       m_neighbors=config['SampleProcessing']['SMOTE_neighbors'],
                        kind='borderline1',
                        n_jobs=N_jobs)
-            X_train, Y_train = sm.fit_sample(X_train, Y_train)
+
+            # First, replace the NaNs:
+            for pnum, (pid, X) in enumerate(zip(patient_IDs_train, X_train_temp)):
+                for fnum, (f, l) in enumerate(zip(X, feature_labels)):
+                    if np.isnan(f):
+                        print("[PREDICT WARNING] NaN found, patient {}, label {}. Replacing with zero.").format(pid, l)
+                        X_train_temp[pnum, fnum] = 0
+            X_train, Y_train = sm.fit_sample(X_train_temp, Y_train)
+            X_train = [(x.tolist(), feature_labels) for x in X_train]
 
         # Find best hyperparameters and construct classifier
         config['HyperOptimization']['use_fastr'] = use_fastr

PREDICT/imagefeatures/log_features.py
@@ -17,8 +17,9 @@
 import PREDICT.imagefeatures.histogram_features as hf
 import PREDICT.helpers.image_helper as ih
 # import imagefeatures.histogram_features as hf
-# import helpers.image_helper as ih
+# import PREIDCT.helpers.sitk_helper as sh
 import SimpleITK as sitk
+import numpy as np
 
 N_BINS = 50
 
@@ -26,6 +27,7 @@ N_BINS = 50
 def get_log_features(image, mask, parameters=dict()):
     # Alternatively, one could use the pxehancement function
     image = sitk.GetImageFromArray(image)
+    # mask = sh.GetImageFromArray(mask.astype(np.uint8))
     if "sigma" in parameters.keys():
         sigma = parameters["sigma"]
     else:
@@ -38,11 +40,17 @@ def get_log_features(image, mask, parameters=dict()):
     # Create LoG filter object
     LoGFilter = sitk.LaplacianRecursiveGaussianImageFilter()
     LoGFilter.SetNormalizeAcrossScale(True)
+
+    # Iterate over sigmas
     for i_index, i_sigma in enumerate(sigma):
-        # Compute LoG Filter image
         LoGFilter.SetSigma(i_sigma)
-        LoG_image = LoGFilter.Execute(image)
-        LoG_image = sitk.GetArrayFromImage(LoG_image)
+        LoG_image = np.zeros(image.GetSize())
+
+        # Iterate over slices
+        for i_slice in range(0, image.GetSize()[0]):
+            # Compute LoG Filter image
+            LoG_image_temp = LoGFilter.Execute(image[i_slice, :, :])
+            LoG_image[i_slice, :, :] = sitk.GetArrayFromImage(LoG_image_temp)
 
         # Get histogram features of LoG image for full tumor
         masked_voxels = ih.get_masked_voxels(LoG_image, mask)

PREDICT/imagefeatures/vessel_features.py
@@ -60,7 +60,8 @@ def get_vessel_features(image, mask, parameters=dict()):
         # Compute Frangi Filter image
         Frangi_image = np.zeros(image.shape)
         for i_slice in range(0, image.shape[2]):
-            Frangi_image[:, :, i_slice] = frangi(image[:, :, i_slice], scale_range=i_sr, scale_step=i_ss)
+            # Note: conversion to uint8, as skimage cannot filter certain float images
+            Frangi_image[:, :, i_slice] = frangi(image[:, :, i_slice].astype(np.uint8), scale_range=i_sr, scale_step=i_ss)
 
         # Get histogram features of Frangi image for full tumor
         masked_voxels = ih.get_masked_voxels(Frangi_image, mask)